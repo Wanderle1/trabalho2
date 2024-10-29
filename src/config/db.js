e98f9d9e4bdec39f16a76160e00dc3b39ad344c4
Wanderle1 <wanderlei.mora3s@gmail.com>
2024-10-29 14:34:23 -0300

    first

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const connectDB = async () => {
-    mongoose.connect('mongodb://localhost:27017/').then(() => console.log('connected to MongoDB')).catch((error) => console.error(error))
-}
+     mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`).then(() => console.log('connected to MongoDB')).catch((error) => console.error(error))
+    }
 
 export default connectDB
